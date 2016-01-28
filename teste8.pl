b4c51169ef4ca6fc309557319bab475c1ccbae51
marcelomendoncasoares <marcelo.msoares@poli.ufrj.br>
2016-01-28 18:46:11 -0200

    Atualização com contador de emoticons funcionando e primeira biblioteca feita.

--- teste8.pl
+++ teste8.pl
@@ -1,4 +1,5 @@
 use warnings;
+use POSIX; # Para usar a função 'ceil'
 
 my $linha = "05/16/2015, 09:59 - Camilla Matias de Azevedo: ã 😵      😱 õ Á  À Í @ # 😁 ! ° / ? \\ - _ Ó É Ú Õ Ã ç ~ ^ Ç Eu perdi ª § º😊 hora de ir pro curso do IEG 😭😭😭 - \$ \% \* \& \@ \! com hífen e á à é ó óóóó ü : dois pontos";
 
@@ -9,35 +10,27 @@ my ($interlocutorAtual, $msgAtual) = split(/[:](.*)/, $resto);
 $interlocutorAtual =~ s/.//;
 $msgAtual =~ s/.//;
 
-# Regex use (http://www.comp.leeds.ac.uk/Perl/sandtr.html)
-
-# Range de caracteres (http://www.fileformat.info/info/unicode/block/index.htm)
-#
-# - Latim: 0080 - 00FF (incluindo caracteres com acento) [http://www.fileformat.info/info/unicode/block/latin_supplement/list.htm]
-# - Miscellaneous Symbols and Pictographs: 1F300 - 1F5FF [http://www.fileformat.info/info/unicode/block/miscellaneous_symbols_and_pictographs/index.htm]
-# - Emoticons: 1F600 - 1F64F [http://www.fileformat.info/info/unicode/block/emoticons/index.htm]
-
-# Consegue remover todos os caracteres de texto, deixando os emoticons, mas ainda deixando os caracteres com acento
-# A expressão abaixo é quase como $msgAtual =~ s/[\w]+//g, exceto que também tira ':' e '-' 
+#Remove todo caracter que não é emoticon
 $msgAtual =~ s/[^\s\x{0080}-\x{00FF}]+//g;
-#$msgAtual =~ s/[\x{0080}-\x{008A}]+//;
 
 @arrayOfCharacteresToRemove = ('à', 'á', 'é', 'í', 'ó', 'ú', 'ã', 'õ', 'ü', 'À', 'Á' , 'É', 'Í', 'Ó', 'Ú', 'Ã', 'Õ', 'Ü', 'ç', 'Ç', 'ª', 'º', '°', '§');
-
 foreach (@arrayOfCharacteresToRemove) {
 	$msgAtual =~ s/$_//g;
 }
 
-
 my @arrayPalavras = split(/[\s]+/, $msgAtual);
-##print scalar(@arrayPalavras), "\n";
 
+# A contagem deve ser manual para identificar emoticons colados um a um, e não como palavras.
+# Cada emoticon normal, do tipo 😵, tem 4 bits, mas os emoticons do tipo  tem apenas 3 bits (vamos ignorar isso)
 $contador = 0;
 foreach(@arrayPalavras) {
 	if ($_ ne "") {
 		$contador += length($_)/4;
 	}
 }
+
+#Arredonda para cima o contador, que fará com que a gente só tenha erro na contagem de emoticons de 1 pra cada 4 emoticons de 3 bits, que ainda são menos comuns
+$contador = ceil($contador);
 
 print ("\nQuantidade de emoticons = ", $contador, "\n");
 
