9c3491e2bbb3035ac6216a6b5cc4daaddbf0aa04
marcelomendoncasoares <marcelo.msoares@poli.ufrj.br>
2016-03-06 00:00:10 -0300

    Código de integração do perl e C++ perfeitamente funcional

---
+++ perl/analisaConversa.pl
@@ -0,0 +1,153 @@
+#
+# Linguagens de Programação
+# Professor Miguel Campista
+#
+# Trabalho 2: Módulo Principal do Perl
+# Autores: Lucas Carvalho e Marcelo Soares
+#
+# Este módulo é responsável por processar o arquivo de texto de uma conversa entre duas pessoas e retornar para o módulo C++:
+# 
+# Quem inicia a conversa;
+# Quantidade de palavras por mensagens;
+# Quantidade de mensagens enviadas por vez;
+# Quantidade de emoticons utilizados;
+# Quantidade total de mensagens trocadas na conversa;
+# Quantidade total de palavras escritas
+# 
+
+use 5.010; # Para habilitar a função usada da biblioteca Time::Piece
+use strict;
+use warnings;
+use Time::Piece;
+
+# Habilita a inclusão do diretório atual como caminho '/lib' para a busca das bibliotecas locais
+use File::Basename qw(dirname);
+use Cwd  qw(abs_path);
+use lib dirname(dirname abs_path $0) . '/lib';
+
+# Inclusão dos módulos do código perl
+use bibliotecas::contadorEmoticons qw(contaEmoticons);
+use bibliotecas::tratamentoLinhaTexto qw(trataLinhaTexto);
+use bibliotecas::impressaoResultados qw(imprimeResultados);
+
+sub analisaConversa {
+
+	# Pega o nome do arquivo como argumento passado ao programa em Perl
+	my $nomeArquivo = $_[0];
+
+	# Se não for passado o nome do arquivo, encerra o programa com o erro equivalente
+	($nomeArquivo) or die "--ERRO: não foi passado nome de arquivo texto como argumento.\n\n O programa deve receber o nome de um arquivo texto de conversa do whatsapp entre duas pessoas como argumento.\n";
+
+	my $HORAS_MIN = 10; # Número de horas para ser considerado uma nova conversa 
+
+	# Declaração das variáveis globais que serão utilizadas para o cálculo das saídas do programa
+	# Cada array guarda uma variável para cada interlocutor
+
+	my $interlocutor1 = "";
+	my $interlocutor2 = "";
+	my @qtdPalavras = (0, 0);
+	my @qtdEmoticonsTexto = (0, 0);
+	my @qtdEmoticonsEmoji = (0, 0);
+	my @qtdMsg = (0, 0);
+	my @qtdBlocosMsg = (0, 0); # Blocos de mensagens ininterruptos
+	my @qtdVezesIniciouConversa = (0, 0);
+
+	# Verifica se o arquivo é vazio antes de abrir
+	!(-z $nomeArquivo) or die "--ERRO: o arquivo está vazio.\n\n O programa deve receber o nome de um arquivo texto de conversa do whatsapp entre duas pessoas como argumento.\n";
+	open(my $arquivo, "<", $nomeArquivo) or die "Couldn't open file , $!";
+
+	# Declaração das variáveis utilizadas no loop while de varredura do arquivo
+	my ($dataMsgAnterior, $dataMsgAtual, $msgAtual, $interlocutorAtual, $indice, $indiceAnterior);
+
+	# Processa o arquivo linha a linha, sabendo que cada linha corresponde a uma mensagem
+	while (my $linha = <$arquivo>) {
+
+		# Remove newLine character
+		chomp($linha);
+
+		# Pula linhas vazias 
+		while ($linha =~ /^$/) {
+			$linha = <$arquivo>;
+		}
+
+		# Se a linha não começar com data, apenas adiciona as palavras ao mesmo interlocutor
+		if ($linha =~ /^\d\d\/\d\d\/\d\d\d\d/) {
+			($dataMsgAtual, $interlocutorAtual, $msgAtual) = trataLinhaTexto($linha);
+		}
+		else {
+			# Não precisa do 'our' porque nunca será uma declaração: o arquvo nunca terá a primeria linha não começando com data.
+			$msgAtual = $linha;
+		}
+
+		# Inicializa a dataMsgAnterior para o caso da primeira mensagem
+		if (!$dataMsgAnterior) {
+			$dataMsgAnterior = $dataMsgAtual;
+		}
+
+		# Declara o interlocutor1 e interlocutor2 se não estiverem declarados ainda, colocando o anterior como sendo o próprio atual
+		if (!$interlocutor1) {
+			$interlocutor1 = $interlocutorAtual;
+			$qtdVezesIniciouConversa[0]++;
+			$qtdBlocosMsg[0]++;
+			$indiceAnterior = 0; # Valor do índice para o interlocutor1
+		}
+		elsif (!$interlocutor2 && ($interlocutorAtual ne $interlocutor1)){
+			$interlocutor2 = $interlocutorAtual;
+		}
+
+		# Define qual o interlocutor atual para o caso de ambos já haverem sido declarados
+		if ($interlocutorAtual eq $interlocutor1) {
+			$indice = 0;
+		}
+		else {
+			$indice = 1;	
+		}
+
+		# Verificação de diferença de data para incrementar quem começa a nova conversa
+		my $diferencaDataEmHoras = ($dataMsgAtual - $dataMsgAnterior)/3600;
+		
+		if ($diferencaDataEmHoras > $HORAS_MIN) {
+			$qtdVezesIniciouConversa[$indice]++;
+		}
+
+		# Incrementa a quantidade de mensagens para o interlocutorAtual
+		$qtdMsg[$indice]++;
+
+		# Conta a quantidade de palavras na mensagem e incrementa no contador do interlocutorAtual
+		my @msgAtualArrayPalavras = split(/[\s]+/, $msgAtual);
+		$qtdPalavras[$indice] += scalar(@msgAtualArrayPalavras);
+
+		# Contador de blocos de mensagens ininterruptos (conta +1 para o interlocutor anterior sempre que mudar de interlocutor)	
+		if ($indice != $indiceAnterior) {
+			$qtdBlocosMsg[$indiceAnterior]++;
+		}
+
+		# Contador de emoticon por interlocutor
+		my ($temp1, $temp2) = contaEmoticons($msgAtual);
+
+		$qtdEmoticonsTexto[$indice] += $temp1;
+		$qtdEmoticonsEmoji[$indice] += $temp2;
+
+		$indiceAnterior = $indice;
+		$dataMsgAnterior = $dataMsgAtual;
+	}
+
+	close($arquivo) or die "Couldn't close file properly";
+
+#	imprimeResultados($interlocutor1, $interlocutor2, \@qtdVezesIniciouConversa, \@qtdMsg, \@qtdPalavras, \@qtdEmoticonsTexto, \@qtdEmoticonsEmoji, \@qtdBlocosMsg);
+
+	return ($qtdBlocosMsg[1],
+			$qtdEmoticonsEmoji[1],
+			$qtdEmoticonsTexto[1],
+			$qtdPalavras[1],
+			$qtdMsg[1],
+			$qtdVezesIniciouConversa[1],
+			$qtdBlocosMsg[0],
+			$qtdEmoticonsEmoji[0],
+			$qtdEmoticonsTexto[0],
+			$qtdPalavras[0],
+			$qtdMsg[0],
+			$qtdVezesIniciouConversa[0],
+			$interlocutor2, 
+			$interlocutor1);
+}
---
+++ perl/perlIntegrer.cpp
@@ -0,0 +1,133 @@
+/*
+ *
+ * Linguagens de Programação
+ * Professor Miguel Campista
+ *
+ * Trabalho 3: Integração com Módulo do Perl
+ * Autores: Lucas Carvalho e Marcelo Soares
+ *
+ * Este módulo é responsável por integrar o módulo perl e o módulo C++, chamando o processamento de um arquivo de texto:
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <EXTERN.h>
+#include <perl.h>
+
+using namespace std;
+
+static PerlInterpreter *my_perl;
+
+EXTERN_C void xs_init (pTHX);
+
+EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
+
+EXTERN_C void
+xs_init(pTHX)
+{
+	const char *file = __FILE__;
+	dXSUB_SYS;
+
+	/* DynaLoader is a special case */
+	newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+}
+
+static void teste(string nomeArquivo) {
+
+	dSP;																	// Inicializa o ponteiro da pilha
+	ENTER;																	// Tudo criado à partir daqui...
+	SAVETMPS;																// ...são variáveis temporárias
+	PUSHMARK(SP);															// Lembra o ponteiro para a pilha
+	XPUSHs(sv_2mortal(newSVpv(nomeArquivo.c_str(), nomeArquivo.length())));	// Coloca o nomeArquivo na pilha
+	PUTBACK;																// Transforma o ponteiro local para a pilha em global
+	int numberElemntsOfStack = perl_call_pv("analisaConversa", G_ARRAY);	// Chama a função em Perl
+	SPAGAIN;																// Atualiza o ponteiro para a pilha
+
+	// A partir daqui, os valores de retorno do código Perl são retirados da pilha para as váriáveis do código C++
+	
+//	cout << "Existem " << numberElemntsOfStack << " elementos na pilha." << endl;
+
+	string interlocutor1 = POPp;
+	string interlocutor2 = POPp;
+	
+	// Os resultados são gravados no array segundo a seguinte ordem:
+	// qtdVezesIniciouConversa, qtdMsg, qtdPalavras, qtdEmoticonsTexto, qtdEmoticonsEmoji, qtdBlocosMsg.
+	//
+	// Os resultados são gravados nesta ordem para um dos interlocutores primeiro, e depois para o outro.
+	// Ou seja, os primeiros 6 são números do array são do interlocutor1, e os restantes são do interlocutor2.
+
+	int resultArray[numberElemntsOfStack];
+	for (numberElemntsOfStack -= 2; numberElemntsOfStack > 0; numberElemntsOfStack--) {
+		resultArray[numberElemntsOfStack] = POPi;
+//		cout << "Resultado[" << numberElemntsOfStack << "] = " << resultArray[numberElemntsOfStack] << endl;
+	}
+
+
+	// Se for muito complicado colocar essa função dentro do construtor, pra facilitar, transforma ela em função membro da classe
+	// conversa e chama ela dentro do construtor. Ou ainda tem a possibilidade de fazer dela uma função 'friend'. Digo isso porque
+	// não sei se ela vai se comportar direito sem ser 'static void' (provavelmente vai sem problemas), e ela teria que retornar
+	// muita coisa (duas strings e um array de 12 elementos, que foi declarado local).
+	//
+	// Pra isso, deixei pronto o trecho de código abaixo que pode substituir as quatro linhas de código acima de uma forma mais
+	// organizada para ser utilizada na inicialização dos atributos do objeto 'Conversa'.
+	// 
+	// Minha sugestão é que o objeto 'Conversa' tenha o mesmo formato de atributos abaixo, com a adição das strings dos nomes dos
+	// interlocutores colocadas acima, e com um um atributo adicional (array também) de gênero dos interlocutores, que deve ser
+	// calculado com base nos indicadores daqui após inicialização destes atributos, em uma função à parte.
+
+/*
+	int qtdVezesIniciouConversa[2], qtdMsg[2], qtdPalavras[2], qtdEmoticonsTexto[2], qtdEmoticonsEmoji[2], qtdBlocosMsg[2];
+	for (int index = 0; ((index < 2) && (numberElemntsOfStack > 0)); ++index) {
+
+		qtdVezesIniciouConversa[index] = POPi;
+		qtdMsg[index] = POPi;
+		qtdPalavras[index] = POPi;
+		qtdEmoticonsTexto[index] = POPi;
+		qtdEmoticonsEmoji[index] = POPi;
+		qtdBlocosMsg[index] = POPi;
+		
+	}
+*/
+
+	PUTBACK;
+	FREETMPS;		// Libera os valores de retorno da memória...
+	LEAVE;			// ...e os argumentos "mortal XPUSHed"
+}
+
+
+int main (int argc, char **argv, char **env) {
+
+	// Inicialização
+	char *my_argv[] = { (char *)"", (char *)"analisaConversa.pl" };
+	PERL_SYS_INIT3(&argc, &argv, &env);
+
+	// Criação de um interpretador
+	my_perl = perl_alloc();
+	perl_construct( my_perl );
+	PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+
+	// Invocação do código perl com argumentos
+	perl_parse(my_perl, xs_init, 2, my_argv, (char **)NULL);
+	perl_run(my_perl);
+
+// =============== COLOCAR TUDO ACIMA DESTA LINHA ANTES DA PARTE DE EXECUÇÃO DO CÓDIGO PRINCIPAL ===============
+
+	string nomeArquivo = "../conversas/conversaAilma.txt";
+
+	// Chamada da função em C++ que utiliza o código em Perl
+
+	teste(nomeArquivo);
+
+
+// ================== COLOCAR TUDO ABAIXO DESTA LINHA NO FINAL DO CÓDIGO, APÓS TODA EXECUÇÃO ===================
+
+	// Limpeza do programa
+	perl_destruct(my_perl);
+	perl_free(my_perl);
+
+	// Término do programa
+	PERL_SYS_TERM();
+
+	cout << "Programa terminado com sucesso." << endl;
+ }
