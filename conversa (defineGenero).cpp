27d6c3c696efec995de702614499b2572fc9f4ab
marcelomendoncasoares <marcelo.msoares@poli.ufrj.br>
2016-03-07 01:34:03 -0300

    Classe define gênero

--- conversa (defineGenero).cpp
+++ conversa (defineGenero).cpp
@@ -4,109 +4,61 @@
 #include <vector>
 #include "conversa.h"
 
-Conversa::Conversa (){
-	inserir(); 
-
-}
 
 // Compara em ordem de disparidade encontrada
 void Conversa::defineGenero(interlocutor1, interlocutor2) {
 
-	// Foi dada uma pontuação por cada critério, que um ou outro interlocutor ganha por ser maior, a ser:
-	// qtdMsg vale 5
-	// qtdEmojis vale 5
-	// qtdVezesIniciouConversa vale 3
-	// qtdEmoticonsTexto vale 1
-	// qtd
+	// Foi dada uma pontuação por cada critério, que um ou outro interlocutor ganha por ser maior.
 	// A maior pontuação indica o interlocutor de sexo feminino.
+	// A variável qtdBlocosMsg é usada apenas no relatório, não importando aqui.
+
+	const int PONTOS_MSG = 4
+	const int PONTOS_EMOJIS = 4
+	const int PONTOS_VEZES_INICIOU_CONVERSA = 3
+	const int PONTOS_EMOTICONS_TEXTO = -1
+	const int PONTOS_PALAVRAS = 1
 
 
+	int pontuacao[2] = (0, 0);
 
-	// As maiores disparidades de gênero ocorreram com quantidade de mensagens e de emojis enviados (feminino > masculino)
-	if ((qtdMsg[0] > qtdMsg[1]) && (qtdEmoticonsEmoji[0] > qtdEmoticonsEmoji[1])) {
-		
+	// Comparação de maior quantidade de mensagens
+	if (qtdMsg[0] > qtdMsg[1]) 
+		pontuacao[0] += PONTOS_MSG;
+	else
+		pontuacao[1] += PONTOS_MSG;
+
+	// Comparação de maior quantidade de emoticons emoji enviados
+	if (qtdEmoticonsEmoji[0] > qtdEmoticonsEmoji[1])
+		pontuacao[0] += PONTOS_EMOJIS;
+	else
+		pontuacao[1] += PONTOS_EMOJIS;
+
+	// Comparação de maior quantidade de vezes que iniciou conversa, como estabelecido segundo critério no arquivo analisaConversa.pl
+	if (qtdVezesIniciouConversa[0] > qtdVezesIniciouConversa[1])
+		pontuacao[0] += PONTOS_VEZES_INICIOU_CONVERSA;
+	else
+		pontuacao[1] += PONTOS_VEZES_INICIOU_CONVERSA;
+
+	// Comparação de maior quantidade de emoticons texto (sexo masculino predominantemente)
+	if (qtdEmoticonsTexto[0] > qtdEmoticonsTexto[1])
+		pontuacao[0] += PONTOS_EMOTICONS_TEXTO;
+	else
+		pontuacao[1] += PONTOS_EMOTICONS_TEXTO;
+
+	// Comparação de maior quantidade de palavras (não demonstrou influência determinística)
+	if (qtdPalavras[0] > qtdPalavras[1])
+		pontuacao[0] += PONTOS_EMOTICONS_TEXTO;
+	else
+		pontuacao[1] += PONTOS_EMOTICONS_TEXTO;
+
+
+	// Compara as pontuações finais e determina os gêneros na classe conversa
+	if (pontuacao[0] > pontuacao[1]) {
 		interlocutora = interlocutor1;
 		interlocutor = interlocutor2;
 	}
-
-
-	if (qtdVezesIniciouConversa[0] > qtdVezesIniciouConversa[1])
-
-
-
-
-		int qtdVezesIniciouConversa[2];
-		int qtdMsg[2];
-		int qtdPalavras[2];
-		int qtdEmoticonsTexto[2];
-		int qtdEmoticonsEmoji[2];
-		int qtdBlocosMsg[2];
-
-
-}
-
-
-void Conversa::analisarConversa() {
-
-	cout<<"Numero de mensagens: "<< endl;
-
-}
-
-string Conversa::getInterlocutor () {return interlocutor;}
-
-string Conversa::getInterlocutora () {return interlocutora;}
-
-int Conversa::getQtdPalavras () {return qtdPalavras[1];}
-
-int Registro::qtd(){return conversas.at(0)->getQtdPalavras();}
-
-void Registro::inserirRegistro () {
-	string sn;
-	
-	conversas.push_back (new Conversa);
-	cout<<"qtdpalavras: "<<conversas.at(0)->getQtdPalavras()<<endl;
-	cout<<"Deseja gerar um relatorio?\n S-sim	N-nao"<<endl;
-	cin>> sn;
-	if(sn=="sim" || sn=="Sim")
-		gerarRelatorio(1);
-}
-
-void Registro::excluir () {
-	int numeroConversa;
-
-	cout << "Digite o numero da conversa a ser excluida\n"<< endl;
-	exibirConversas();
-	cin >> numeroConversa;
-	if(numeroConversa!=-1)
-		conversas.erase(conversas.begin() + numeroConversa -1);
-
-}
-
-void Registro::exibirConversas () {
-
-	for (int i=0; i < conversas.size(); i++){
-		cout << i +1<< " : " << conversas.at(i)->getInterlocutor() << " e " << conversas.at(i)->getInterlocutora() << "\n" << endl;
+	else {
+		interlocutor = interlocutor1;
+		interlocutora = interlocutor2;	
 	}
-	cout<< "-1 : cancelar"<< endl;
-}
-
-void Registro::exibirAnalise() {
-	int numeroConversa;
-
-	cout << "Digite o numero da conversa a ser analisada\n"<< endl;
-	exibirConversas();
-	cin >> numeroConversa;
-	conversas.at(numeroConversa-1)->analisarConversa();
-}
-
-void Registro::gerarRelatorio(int op) {
-	int numeroConversa;
-	
-	if (op==0){
-		cout << "Digite o numero da conversa\n"<< endl;
-		exibirConversas();
-		cin >> numeroConversa;
-	}
-
-
 }
