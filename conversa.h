fc2475ee5b6b9dfe351929ed1d54b82cafb9145a
lucas <llagocarvalho@gmail.com>
2016-03-07 00:47:46 -0300

    testando funcao

---
+++ conversa.h
@@ -0,0 +1,51 @@
+
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Conversa {
+
+	friend void teste(string);
+
+	public:
+		Conversa();
+		void inserir();
+		string getInterlocutora ();
+		string getInterlocutor ();
+		int getQtdPalavras ();
+		void analisarConversa();
+
+
+
+	private:
+		string arquivo, interlocutor, interlocutora;
+		int qtdVezesIniciouConversa[2];
+		int qtdMsg[2];
+		int qtdPalavras[2];
+		int qtdEmoticonsTexto[2];
+		int qtdEmoticonsEmoji[2];
+		int qtdBlocosMsg[2];
+
+};
+
+
+class Registro {
+
+	public:
+
+
+		void inserirRegistro ();
+		void excluir();
+		void exibirConversas();
+		void exibirAnalise();
+		void gerarRelatorio( int );
+		int qtd();
+
+
+	private:
+
+		vector < Conversa *> conversas;
+};
